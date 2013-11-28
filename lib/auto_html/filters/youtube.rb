6e69fab32266ac58987db65f0e718cf0f60d5975
Yuri Grechka <y.a.sidorov@gmail.com>
2013-11-28 21:05:08 +0300

    Getting youtube video dimensions right from YouTube oembed API.

--- lib/auto_html/filters/youtube.rb
+++ lib/auto_html/filters/youtube.rb
@@ -2,8 +2,14 @@ AutoHtml.add_filter(:youtube).with(:width => 420, :height => 315, :frameborder =
   regex = /https?:\/\/(www.)?(youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/watch\?feature=player_embedded&v=)([A-Za-z0-9_-]*)(\&\S+)?(\S)*/
   text.gsub(regex) do
     youtube_id = $3
-    width = options[:width]
-    height = options[:height]
+    
+    youtube_json = result = JSON.parse(open("http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=#{youtube_id}&format=json").read)
+    
+    width = youtube_json["width"]
+    height = youtube_json["height"]
+    
+    #width = options[:width]
+    #height = options[:height]
     frameborder = options[:frameborder]
 		wmode = options[:wmode]
 		src = "//www.youtube.com/embed/#{youtube_id}"
