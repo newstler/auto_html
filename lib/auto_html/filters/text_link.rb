5502c23f2f67c769ed7378bc44e0a4bd188285a8
Yuri Sidorov <cpu@robotinthecloak.com>
2014-03-15 18:08:12 +0400

    Updated text link.

--- lib/auto_html/filters/text_link.rb
+++ lib/auto_html/filters/text_link.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:text_link) do |text|
-  text.gsub(/^\\https?:\/\//) do |match|
+  text.gsub(/^\\https?:/) do |match|
     %|#{match.to_s[1..-1]}<span class="hide"></span>|
   end
 end
