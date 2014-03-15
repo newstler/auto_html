6b32a6811e1531067830be6d464971ba9cfa7f71
Yuri Sidorov <cpu@robotinthecloak.com>
2014-03-15 14:55:23 +0400

    Added text link.

--- lib/auto_html/filters/text_link.rb
+++ lib/auto_html/filters/text_link.rb
@@ -1,5 +1,5 @@
 AutoHtml.add_filter(:text_link) do |text|
-  text.gsub(/^_https?:\/\//) do |match|
+  text.gsub(/_((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/) do |match|
     %|#{match.to_s[1..-1]}<span class="hide"></span>|
   end
 end
